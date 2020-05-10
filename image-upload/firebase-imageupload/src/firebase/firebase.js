f8f90189e337a693cf0f934759825de997444c0e
Bhakti Budiman Novanda <myforework@live.com>
2020-05-10 09:30:07 +0700

    Update firebase credential

--- image-upload/firebase-imageupload/src/firebase/firebase.js
+++ image-upload/firebase-imageupload/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/storage'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDgzuz2qHRQd6pjvkk2ULp-wLKlDfg_zK4",
-    authDomain: "cloudstore-4cb34.firebaseapp.com",
-    databaseURL: "https://cloudstore-4cb34.firebaseio.com",
-    projectId: "cloudstore-4cb34",
-    storageBucket: "cloudstore-4cb34.appspot.com",
-    messagingSenderId: "357748634759",
-    appId: "1:357748634759:web:20af6aab611f61e2a33422"
+    apiKey: "<your api key>",
+    authDomain: "<your auth domain>",
+    databaseURL: "<your db url>",
+    projectId: "<your project id>",
+    storageBucket: "<your storage bucket>",
+    messagingSenderId: "<your messaging sender id>",
+    appId: "your app id"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
